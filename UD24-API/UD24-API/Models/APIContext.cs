dea60ac64538eba92371e4645fcd87dbe20e9bd8
Weixin <weixin.xu.2018@gmail.com>
2021-02-08 00:47:31 +0100

    Ej1 acabado

---
+++ UD24-API/UD24-API/Models/APIContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UD24_API.Models
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext> options)
+            : base(options) { }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
